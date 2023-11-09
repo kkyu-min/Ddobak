0660794d0b7423247259f1410edc9b6473d0ba68
김진주 <zizu4256@naver.com>
2023-11-09 14:39:42 +0900

    <fix>[#199-KJJ] 폰트보기 페이지-불필요한 코드 삭제

--- frontend/src/pages/fontListPage/FontListPage.tsx
+++ frontend/src/pages/fontListPage/FontListPage.tsx
@@ -7,7 +7,7 @@ import { axiosWithAuth, axiosWithoutAuth } from 'https/http';
 import { getData } from 'https/http';
 // import MiniManuscript from './fontListPageComponents/MiniManuscript';
 
-// API로부터 받아올 폰트 데이터의 타입을 정의
+// API로부터 받아올 데이터 타입 정의
 // type FontList = {
 //   fontCount: number;
 //   fontResponseList: Font[];
@@ -25,7 +25,6 @@ const FontListPage: React.FC = () => {
 
   const [fonts, setFonts] = useState<Font[]>([]); // 폰트 데이터를 위한 상태
 
-  const [showFilterOptions, setShowFilterOptions] = useState(false);
   // const [currentPage, setCurrentPage] = useState(1); // 현재 페이지 상태
 
   // 컴포넌트 마운트시 API 호출
@@ -33,7 +32,6 @@ const FontListPage: React.FC = () => {
     const fetch = async () => {
       const token = await getData('accessToken');
       if (!token) {
-        // 토큰 있음
         try {
           const response = await axiosWithoutAuth.get('/font/list/NoAuth')
             .then((r) => { return r; });
@@ -71,7 +69,7 @@ const FontListPage: React.FC = () => {
     }
   };
 
-  // 현재 페이지의 폰트 목록을 렌더링
+  // 폰트보기 페이지의 폰트 목록 렌더링
   const renderFontBoxes = () => {
     return fonts.map((font) => (
       <FontBoxComponent
@@ -110,7 +108,7 @@ const FontListPage: React.FC = () => {
     '아이같은',
     '자유로운',
   ];
-
+  const [showFilterOptions, setShowFilterOptions] = useState(false);
   const [checkedOptions, setCheckedOptions] = useState<string[]>([]);
   const [searchTerm, setSearchTerm] = useState<string>('');
 
@@ -137,35 +135,36 @@ const FontListPage: React.FC = () => {
       </div>
     );
   };
-  // search page size
+  // page size
   // 폰트 데이터 필터링
   const fetchFilteredFonts = useCallback(async () => {
     console.log('선택된 필터 옵션:', checkedOptions);
     console.log('입력한 검색어:', searchTerm);
     try {
       const params = {
-      search: searchTerm,
-      keywords: checkedOptions.length > 0 
-      ? checkedOptions.join(',')  // 선택된 옵션이 있을 경우, 쉼표로 구분된 문자열로 전송
-      : {}  // 선택된 옵션이 없을 경우, params를 비워서 모든 데이터 요청
+        search: searchTerm,
+        keywords: checkedOptions.length > 0
+          ? checkedOptions.join(',')  // 선택된 옵션이 있을 경우, 쉼표로 구분된 문자열로 전송
+          : {}  // 옵션 체크 해제 시, params 비워서 모든 데이터 요청
       }
-        const response = await axiosWithoutAuth.get('/font/list/NoAuth', { params });
-        if (response.data) {
-          console.log('폰트 목록:', response.data);
-          setFonts(response.data.fontResponseList)
-        }
-      } catch (error) {
-        console.error('폰트 목록을 가져오는데 실패했습니다:', error);
+      const response = await axiosWithoutAuth.get('/font/list/NoAuth', { params });
+      if (response.data) {
+        console.log('폰트 목록:', response.data);
+        setFonts(response.data.fontResponseList)
       }
-    }, [searchTerm, checkedOptions]);
-  // 검색어나 체크박스 변경 시 필터링된 폰트 데이터 요청
+    } catch (error) {
+      console.error('폰트 목록을 가져오는데 실패했습니다:', error);
+    }
+  }, [searchTerm, checkedOptions]);
+
+  // 검색어나 옵션 변경 시 필터링된 데이터 요청
   useEffect(() => {
     const timer = setTimeout(() => {
       fetchFilteredFonts();
     }, 500); // 타이핑을 멈춘 후 500ms 뒤 검색 수행
-  
-    return () => clearTimeout(timer); // 클린업 함수로 타이머를 제거합니다.
-  }, [searchTerm, fetchFilteredFonts]); 
+
+    return () => clearTimeout(timer); // 클린업 함수로 타이머를 제거
+  }, [searchTerm, fetchFilteredFonts]); // 의존성 배열 추가
 
   return (
     <>
@@ -179,9 +178,7 @@ const FontListPage: React.FC = () => {
               type="text"
               placeholder="폰트명, 제작자 검색"
               value={searchTerm}
-              onChange={(e) => {
-                setSearchTerm(e.target.value);
-              }}
+              onChange={(e) => setSearchTerm(e.target.value)}
             />
             <FaSistrix size={24} color="black" />
           </div>
@@ -198,7 +195,7 @@ const FontListPage: React.FC = () => {
                 className={`${classes.filterIcon} ${showFilterOptions ? classes.filterIconActive : ''}`}
               />
             </div>
-            {showFilterOptions && renderFilterOptions()}
+            {renderFilterOptions()}
           </div>
         </div>
         <div className={classes.fontBoxContainer}>{renderFontBoxes()}</div>
